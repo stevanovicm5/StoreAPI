782e281f833299b921e9377e8d81f1a243bf4f1e
stevanovicm5 <stevanovicm1008@gmail.com>
2026-03-16 13:08:23 +0100

    Add DTOs

---
+++ BusinessLogicLayer/DTOs/User/UpdateUserDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BusinessLogicLayer.DTOs.User;
+
+public class UpdateUserDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+}
