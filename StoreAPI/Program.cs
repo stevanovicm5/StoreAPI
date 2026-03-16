4802920fb802ced299c0b97aa3cd022fd7a0e35a
stevanovicm5 <stevanovicm1008@gmail.com>
2026-03-16 13:03:08 +0100

    fix: only run Database.Migrate() in development environment

--- StoreAPI/Program.cs
+++ StoreAPI/Program.cs
@@ -37,8 +37,9 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
+if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
