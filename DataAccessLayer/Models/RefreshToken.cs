79e8d943ce21e931ba717894c779f6ca57aefee0
stevanovicm5 <stevanovicm1008@gmail.com>
2026-03-16 21:30:06 +0100

    add RefreshToken model and migration

--- DataAccessLayer/Models/RefreshToken.cs
+++ DataAccessLayer/Models/RefreshToken.cs
@@ -1,7 +1,11 @@
-
 namespace DataAccessLayer.Models;
 
 public class RefreshToken
 {
-    
+    public Guid Id { get; set; }
+    public string Token { get; set; } = null!;
+    public DateTime ExpiresAt { get; set; }
+    public bool IsRevoked { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; } = null!;
 }
